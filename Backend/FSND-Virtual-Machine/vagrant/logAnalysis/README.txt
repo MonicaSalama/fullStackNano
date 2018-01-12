a2c78d500856f24b9bee674b19614cee4443ef9c
Monica Salama <monicasalama6@gmail.com>
2018-01-12 21:44:39 +0200

    .

--- Backend/FSND-Virtual-Machine/vagrant/logAnalysis/README.txt
+++ Backend/FSND-Virtual-Machine/vagrant/logAnalysis/README.txt
@@ -1,5 +1,35 @@
-- Command to create view :
-  query = create view logArticles as select split_part(path, '/', 3) as path from log where path LIKE '/article/%;
+- Log Analysis Project
+Internal reporting tool that will use information from the database to discover what kind of articles the site's readers like.
+The database contains newspaper articles, as well as the web server log for the site.
+The log has a database row for each time a reader loaded a web page.
+Using that information, the program will answer questions about the site's user activity.
+The program will connect to the database, use SQL queries to analyze the log data, and print out the answers to some questions.
+Questions the tool answers:
+1. What are the most popular three articles of all time?
+2. Who are the most popular article authors of all time?
+3. On which days did more than 1% of requests lead to errors?
 
-- To run the program :
-  python log.py
+Prerequisites
+1. python 2
+2. vagrant
+
+
+How to run:
+1. Move to Log Analysis Project directory
+2. python log.py
+3. Check output.txt
+
+Output example:
+  1. Most popular three articles of all time:
+  	- "Candidate is jerk, alleges rival" - 338647 views
+  	- "Bears love berries, alleges bear" - 253801 views
+  	- "Bad things gone, say good people" - 170098 views
+
+  2. Most popular article authors of all time:
+  	- Ursula La Multa - 507594 views
+  	- Rudolf von Treppenwitz - 423457 views
+  	- Anonymous Contributor - 170098 views
+  	- Markoff Chaney - 84557 views
+
+  3. Days on which more than 1% of requests lead to errors:
+  	- 2016-07-17 - 2.25% errors
--- Backend/FSND-Virtual-Machine/vagrant/logAnalysis/log.py
+++ Backend/FSND-Virtual-Machine/vagrant/logAnalysis/log.py
@@ -1,33 +1,53 @@
+#!/usr/bin/env python2
 import psycopg2
 
 DBNAME = 'news'
 
+"""Internal reporting tool to perform 3 functionalities.
+- Get most popular three articles of all time.
+- Get most popular get most popular authors.
+- Report days where more than 1% of the requests had errors.
+"""
+
 
 def get_connection():
+    """Creates database connection.
+    """
+
     db = psycopg2.connect(dbname=DBNAME, host="localhost",
                           user="vagrant", password="vagrant")
+    # db = psycopg2.connect(dbname=DBNAME)
     return db
 
 
 def close_connection(db):
+    """Closes database connection.
+    """
+
     db.close()
 
 
-def get_most_three_popular_articles(cursor):
-    query = """select path, count(*)
-               from (select * from logArticles) as articleNames
-               group by path
-               order by count(*) DESC limit 3;"""
+def get_most_three_popular_articles(cursor, out_file):
+    """Reports most popular three articles.
+    """
+
+    query = """select articles.title, count(*)
+               from (select * from logArticles) as articlePath join articles
+               on articlePath.path = articles.slug
+               group by articles.title
+               order by count(*) DESC limit 3"""
 
     cursor.execute(query)
     results = cursor.fetchall()
-    print('Most popular three articles of all time:')
+    out_file.write('1. Most popular three articles of all time:\n')
     for tup in results:
-        print('\t- \"%s\" - %s views' % (tup[0], str(tup[1])))
-    print('\n')
+        out_file.write('\t- \"%s\" - %s views\n' % (tup[0], str(tup[1])))
+    out_file.write('\n')
 
 
-def get_most_popular_authors(cursor):
+def get_most_popular_authors(cursor, out_file):
+    """Reports most popular authors.
+    """
     subquery1 = """select authors.name, articles.slug
                    from authors join articles
                    on authors.id = articles.author"""
@@ -40,13 +60,16 @@ def get_most_popular_authors(cursor):
 
     cursor.execute(query)
     results = cursor.fetchall()
-    print('Most popular article authors of all time:')
+    out_file.write('2. Most popular article authors of all time:\n')
     for tup in results:
-        print('\t- %s - %s views' % (tup[0], str(tup[1])))
-    print('\n')
+        out_file.write('\t- %s - %s views\n' % (tup[0], str(tup[1])))
+    out_file.write('\n')
 
 
-def get_days_with_more_than_1_percentage_errors(cursor):
+def get_days_with_more_than_1_percentage_errors(cursor, out_file):
+    """Report days where more than 1% of the requests had errors
+    """
+
     q1 = """select DATE(time), count(*) as cerror
             from log
             where status LIKE '404%'
@@ -63,15 +86,22 @@ def get_days_with_more_than_1_percentage_errors(cursor):
 
     cursor.execute(query)
     results = cursor.fetchall()
-    print('Days on which more than 1% of requests lead to errors:')
+    out_file.write('3. Days on which more than 1% of requests lead to errors:\n')
     for tup in results:
-        print('\t- %s - %.2f%% errors' % (tup[0], tup[1]))
+        out_file.write('\t- %s - %.2f%% errors\n' % (tup[0], tup[1]))
 
 
 if __name__ == '__main__':
     db = get_connection()
     c = db.cursor()
-    get_most_three_popular_articles(c)
-    get_most_popular_authors(c)
-    get_days_with_more_than_1_percentage_errors(c)
+    # Used query to extrat article slug from the path ex:/article/x -> x
+    create_view = """REPLACE view logArticles as
+                     select split_part(path, '/', 3) as path
+                     from log where path LIKE '/article/%';"""
+
+    out_file = open('output.txt', 'w')
+    get_most_three_popular_articles(c, out_file)
+    get_most_popular_authors(c, out_file)
+    get_days_with_more_than_1_percentage_errors(c, out_file)
+    out_file.close()
     close_connection(db)
